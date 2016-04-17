7c42e9ab1b76efdd972d1db0bee8acab37050ff4
Sean Colandrea <link50@gmail.com>
2016-04-17 19:36:22 -0400

    Updated gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,30 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 var browserSync = require('browser-sync').create()
 var autoprefixer = require('gulp-autoprefixer')
+var useref = require('gulp-useref')
+var uglify = require('gulp-uglify')
+var gulpIf = require('gulp-if')
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/main.scss')
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('build/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
 })
 
-gulp.task('watch', ['browserSync', 'sass'], function() {
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // .pipe(gulpIf('*.jf'), uglify())
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('watch', ['browserSync', 'sass', 'useref'], function() {
   gulp.watch('app/scss/**/*.scss', ['sass'])
+  gulp.watch(['app/js/**/*.js', 'app/*.html'], ['useref'])
   gulp.watch('app/*.html', browserSync.reload)
   gulp.watch('app/js/**/*.js', browserSync.reload)
 })
@@ -22,7 +33,7 @@ gulp.task('watch', ['browserSync', 'sass'], function() {
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir: 'build'
     }
   })
 })
