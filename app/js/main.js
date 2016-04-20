2f13d7e0061add1c3ace1ab37dfd6208f1b13b76
Sean Colandrea <link50@gmail.com>
2016-04-20 11:34:16 -0400

    Adding scroll down animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 new Vivus('sean-logo', {
   duration: 300,
-  pathTimingFunction: Vivus.EASE_IN
+  pathTimingFunction: Vivus.EASE_IN,
+  onReady: function (obj) {
+    obj.el.style.opacity = 1;
+  }
 }, function(obj) {
   obj.el.classList.add('finished');
+  var el = document.getElementById('time-to-scroll');
+  el.classList.add('scroll-time');
 });
